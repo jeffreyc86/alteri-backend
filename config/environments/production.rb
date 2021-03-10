2146a54d732e4a08aed3b854394423dd551a835f
Jeffrey Chiu <mr.jeffrey.chiu@gmail.com>
2021-03-10 09:09:12 -0500

    make it better

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,9 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
+  config.web_socket_server_url = "wss://alteri.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://alteri.herokuapp.com', 'http://alteri-client.netlify.app']
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
